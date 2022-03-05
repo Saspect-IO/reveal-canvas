671d2fdf26bcd9d3aa830f80fa3c9fdbe0bd5563
Sheldon Small <sheldonsmall.io@gmail.com>
2022-03-05 02:09:03 -0500

    use canvas context class, refactor main function and animation method

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,37 @@
-import Painting, { MappedImage } from '@/core/painting';
 import { config } from './config/env';
+import CVContext from './core/cvContext';
+import Painting from '@/core/painting';
 import Particle from './core/particle';
 import Texture from './core/texture';
+import { ParticleConfig, ProgramEntrySettings } from './modules';
 
 (async () => {
-  const imageUrl = config?.IMAGES.DOIST.TEXTURES as string;
-  const particleCount = 25000;
-  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-  let ctx: CanvasRenderingContext2D | null = null;
-  let particleStore: Particle[] = [];
-  let mappedImage: MappedImage[][] = [];
 
-  async function init() {
+  const cvContext = new CVContext(ProgramEntrySettings.CANVAS_ID)
+  cvContext.fitScreen().setColor().setSize().clear()
+  const {canvas, ctx} = cvContext
 
+  const texture = new Texture(canvas, ctx)
+  const img = await texture.loadImage(config?.IMAGES.DOIST.TEXTURES as string)
+  texture.drawImage(img)
+  const pixels = texture.loadImagePixels()
 
-    if (canvas.getContext) {
-      ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-      const texture = new Texture(canvas, ctx);
-      const img = await Texture.loadImage(imageUrl);
-      texture.draw(img);
-      const pixels = texture.loadPixels();
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      const painting = new Painting(canvas, ctx, pixels);
-      mappedImage = painting.getMappedImage();
-      particleStore = Particle.generateParticles(
-        canvas,
-        ctx,
-        particleCount,
-        mappedImage,
-      );
-      animate();
-    }
-  }
+  cvContext.clear()
 
-  init();
+  const painting = new Painting(canvas, ctx, pixels);
+  const particleStore: Particle[] = Particle.generateParticles(
+    canvas,
+    ctx,
+    ParticleConfig.COUNT,
+    painting.getMappedImage(),
+  );
 
   function animate() {
-    (ctx as CanvasRenderingContext2D).globalAlpha = 0.05;
-    (ctx as CanvasRenderingContext2D).fillStyle = 'rgb(0,0,0)';
-    (ctx as CanvasRenderingContext2D).fillRect(
-      0,
-      0,
-      canvas.width,
-      canvas.height,
-    );
     for (let i = 0; i < particleStore.length; i++) {
       particleStore[i].update();
       particleStore[i].draw();
     }
     requestAnimationFrame(animate);
   }
+  animate();
 })();
