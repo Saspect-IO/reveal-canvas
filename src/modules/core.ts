056c1743862f7a94c1d2adda1cae5f35124935e7
Sheldon Small <sheldonsmall.io@gmail.com>
2022-03-05 02:09:27 -0500

    add config constants core module

---
+++ src/modules/core.ts
@@ -0,0 +1,10 @@
+export enum ProgramEntrySettings {
+    CANVAS_ID = 'canvas',
+    CANVAS_CONTEXT = '2d',
+    GLOBAL_ALPHA = 0.05,
+    CANVAS_FILL_STYLE = 'rgb(0,0,0)'
+}
+
+export enum ParticleConfig {
+    COUNT = 25000
+}
